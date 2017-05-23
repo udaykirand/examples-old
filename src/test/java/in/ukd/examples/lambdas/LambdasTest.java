e05c7efe35de2c5a1de0f035a90d864dc7f674e3
udadh <udaykiran.dharbamulla@idainfront.se>
2017-05-23 20:40:11 +0200

    Initial commit

---
+++ src/test/java/in/ukd/examples/lambdas/LambdasTest.java
@@ -0,0 +1,14 @@
+package in.ukd.examples.lambdas;
+
+import org.junit.Test;
+
+/**
+ * Created by udadh on 5/23/2017.
+ */
+public class LambdasTest {
+
+    @Test
+    public void testExamples() {
+
+    }
+}
